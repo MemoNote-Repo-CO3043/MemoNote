08dcae297fab693c6c48dc589fbb3f44b31ec959
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-12-22 11:36:00 +0700

    add login,register api

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -13,25 +13,6 @@ export class UserService {
   async createHash(password: string) {
     return await bcrypt.hash(password, saltRounds);
   }
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
   async login(username: string, password: string) {
     const user = await this.firebaseService.getUser(username);
     if (await bcrypt.compare(password, user.password)) {
